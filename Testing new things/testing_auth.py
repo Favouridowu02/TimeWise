007994525b933ea1644a1dd6772cf2b3c0ac28d2
Favour Idowu <favouridowu02@gmail.com>
2025-03-21 13:18:12 +0100

    feat: Add JWT authentication and user management for testing in TimeWise
    - Set up Flask app with JWTManager and secret key generation
    - Configure SQLite in-memory database for testing
    - Define User model with username and full_name fields
    - Implement user_identity_loader and user_lookup_loader for JWT
    - Add login route to authenticate users and generate access tokens
    - Add protected route to verify JWT authentication and return current user identity
    
    This update introduces JWT authentication and user management functionality for testing purposes in the TimeWise application, including user login and protected routes.

---
+++ Testing new things/testing_auth.py
@@ -0,0 +1,66 @@
+from hmac import compare_digest
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import current_user
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+import secrets
+
+
+app = Flask(__name__)
+
+secret_key = secrets.token_urlsafe()
+
+app.config["JWT_SECRET_KEY"] = secret_key
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+jwt = JWTManager(app)
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Text, nullable=False, unique=True)
+    full_name = db.Column(db.Text, nullable=False)
+
+    def checkpassword(self, password):
+        return compare_digest(password, "password")
+
+
+@jwt.user_identity_loader
+def user_identity_lookup(user):
+    return user.id
+
+
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+    identity = jwt_data["sub"]
+    return User.query.filter_by(id=identity).one_or_none()
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+
+    user = User.query.filter_by(username=username).one_or_none()
+    if not user or not user.check_password(password):
+        return jsonify({"Wrong username or password"}), 401
+    
+    access_token = create_access_token(identity=user)
+    return jsonify(access_token=access_token)
+
+
+@app.route("/who_am_i", methods=["GET"])
+@jwt_required()
+def protected():
+    return jsonify(
+        id=current_user.id,
+        full_name=current_user.full_name,
+        username=current_user.username
+    )
