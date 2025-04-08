5a307eed4a101139c7ad7ce538b1b094d5d0987f
Favour Idowu <favouridowu02@gmail.com>
2025-04-08 12:49:29 +0100

    docs: Add placeholder module for user API endpoints
    - Created `users.py` with a docstring explaining its pending status due to overlapping functionality with `auth.py`.
    - Initialized a `user` blueprint for future user-related API endpoints.

--- Backend/api/v1/views/users.py
+++ Backend/api/v1/views/users.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 """
+    This Module is currently pending because most of the features are conatined in the auth.py module.
     This Module contains the api endpoints for the user
 """
-from flask import Flask, abort, request
+from flask import Flask, abort, request, jsonify, Blueprint
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from models.base_model import db
+from models.user import User
+from datetime import datetime
+import uuid
+
+user_bp = Blueprint('user', __name__)
